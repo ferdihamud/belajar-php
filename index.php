5f497af2be9f9104979dbf10e6d7d26129d62eb5
Ferdiansyah <ferdihamud19@gmail.com>
2022-10-24 11:13:17 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama = "Ferdiansyah";
+$umur = 21;
+
+echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+$namaAdik = "Adel";
+$umurAdik = 18;
+
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia 
+$umurAdik tahun.<br>";
+
+$selisihUmur = $umur - $umurAdik;
+
+echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun.";
+
+?>
